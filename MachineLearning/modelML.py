e9596e6b2f4ad01514884112ec9f43695b6b2a7a
ngocbao220 <ngocbaotrinhtuan@gmail.com>
2024-11-13 08:36:56 +0700

    fucking partner

--- MachineLearning/modelML.py
+++ MachineLearning/modelML.py
@@ -1,70 +1,38 @@
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+from DataProcessing.StandardizeData import Price
 import pandas as pd
-import numpy as np
-import copy, math
 
-class modelML:
-    def __init__(self, X, Y):
-        self.X_train = X
-        self.Y_train = Y
-        self.w = np.zeros(X.shape[1])
-        self.b = 0
+data = pd.read_csv('Data/standardizedData/standardized_data.csv')
 
-    def compute_cost(self, X, Y, w, b):
-        m = X.shape[0]
-        cost = 0.0
-        for i in range(m):
-            f_wb_i = np.dot(X[i], w) + b
-            cost = cost + (f_wb_i - Y[i])**2
-        cost = cost / (2 * m)
+# Predict price of non_exist building
+X = data.drop(['Mức giá (triệu/m²)', 'Mã lịch sử giá'], axis=1)
+y = data['Mức giá (triệu/m²)']
 
-        return cost
-    
-    def compute_gradient(self, X, Y, w, b):
-        m, n = X.shape
-        dj_dw = np.zeros((n,))
-        dj_db = 0.0
+categorical_features = ['Xã/Phường', 'Quận/Huyện', 'Chủ đầu tư', 'Tên dự án', 'Pháp lý', 'Nội thất']
+numerical_features = ['Diện tích (m²)', 'Số phòng ngủ (phòng)', 'Số toilet (phòng)']
 
-        for i in range(m):
-            err = (np.dot(X[i], w) + b) - Y[i]
-            for j in range(n):
-                dj_dw[j] += err * X[i, j]
-            dj_db += err
-        dj_dw = dj_dw / m
-        dj_db = dj_db / m
-        
-        return dj_db, dj_dw
-    
-    def gradient_descent(self, X, y, w_in, b_in, alpha, num_iters): 
-        J_history = []
-        w = copy.deepcopy(w_in)
-        b = b_in
-        
-        for i in range(num_iters):
-            dj_db, dj_dw = self.compute_gradient(X, y, w, b)
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', StandardScaler(), numerical_features),
+        ('cat', OneHotEncoder(handle_unknown='ignore'), categorical_features)
+    ])
 
-            # Cập nhật tham số w, b
-            w = w - alpha * dj_dw
-            b = b - alpha * dj_db
+model = Pipeline(steps=[
+    ('preprocessor', preprocessor),
+    ('regressor', RandomForestRegressor())
+])
 
-            # Lưu chi phí tại mỗi lần lặp
-            if i < 100000:
-                J_history.append(self.compute_cost(X, y, w, b))
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
 
-            # In chi phí sau mỗi khoảng nhất định
-            if i % math.ceil(num_iters / 10) == 0:
-                print(f"Iteration {i:4d}: Cost {J_history[-1]:8.2f}")
-            
-        return w, b, J_history
-    
-X_train = np.array([[1, 2], [3, 4], [5, 6]])  # Ví dụ giả sử bạn có 100 mẫu và 3 đặc trưng
-y_train = np.array([2, 4, 6])     # Ví dụ giả sử mục tiêu có 100 giá trị
+model.fit(X_train, y_train)
 
-modelML = modelML(X_train, y_train)
-w_init = np.zeros(X_train.shape[1])
-b_init = 0
-alpha = 0.01
-iterations = 1000
+y_pred = model.predict(X_test)
+mae = mean_absolute_error(y_test, y_pred)
+print(f'Mean Absolute Error: {mae}')
 
-w_final, b_final, J_history = modelML.gradient_descent(X_train, y_train, w_init, b_init, alpha, iterations)
-print(w_final)
-print(b_final)
+# Predict price of trend in the future

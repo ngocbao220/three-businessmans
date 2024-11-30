61a32223937a54f50b4435abda253779ff8175d3
ntkReyn <khoibg2005@gmail.com>
2024-11-30 18:36:51 +0700

    update code collect view of project

--- DataProcessing/CollectViewProject.py
+++ DataProcessing/CollectViewProject.py
@@ -23,8 +23,6 @@ def get_project_data_dict(historyPriceOfProject):
     if pd.isna(historyPriceOfProject) or historyPriceOfProject.strip() == "Không có dữ liệu lịch sử giá":
         return None
     
-    if pd.isna(historyPriceOfProject):
-        return None
     # Tách các giá trị theo dấu ;
     gia_list = [re.findall(r"(\d+,\d+) tr/m²", gia.strip()) for gia in historyPriceOfProject.split(";") if "Giá" in gia]
     # Loại bỏ các giá trị rỗng hoặc không hợp lệ
@@ -32,10 +30,10 @@ def get_project_data_dict(historyPriceOfProject):
     return gia_list[-1] if gia_list else None
 
 # Tạo cột "Giá cuối" với giá trị cuối cùng
-df["Giá cuối"] = df["Lịch sử giá"].apply(get_project_data_dict)
+df["Giá trung bình"] = df["Lịch sử giá"].apply(get_project_data_dict)
 
 # Lọc ra DataFrame chỉ chứa các hàng có giá trị "Giá cuối" không null
-df_result = df[df["Giá cuối"].notna()][["Tên dự án", "Giá cuối"]]
+df_result = df[df["Giá trung bình"].notna()][["Tên dự án", "Giá cuối"]]
 
 
 def change_name_to_url(ProjectName):
@@ -49,23 +47,24 @@ def change_name_to_url(ProjectName):
 df_result['Tên dự án URL'] = df_result['Tên dự án'].apply(change_name_to_url)
 df_result.drop_duplicates(inplace=True)
 
-print(df_result)
-
+#print(df_result)
 
 list_of_view = []
 
 for url_name in df_result["Tên dự án URL"]:
+
     url = 'https://batdongsan.com.vn/nha-dat-ban-' + url_name
     driver.get(url)
     try:
-        # Tìm phần tử chứa số lượt xem
+        
         element = driver.find_element(By.CLASS_NAME, "re__srp-traffic-label")
         view_text = element.text
-        # Tách số lượt xem từ chuỗi văn bản
+        
         view = int("".join(filter(str.isdigit, view_text)))
         list_of_view.append(view)
+
     except Exception as e:
-        print(f"Không tìm thấy thông tin số lượt xem: {e}")
+        print(f"Không tìm thấy thông tin số lượt xem")
         list_of_view.append(0)
 
 df_result['Lượt xem'] = list_of_view

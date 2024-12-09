20c4a2f71386151b8ab4f54ac25d03bb2b9db397
ntkReyn <khoibg2005@gmail.com>
2024-12-09 21:12:41 +0700

    update

--- HighchartsProject/script.js
+++ HighchartsProject/script.js
@@ -13,11 +13,7 @@ import { set_Scene } from "./events.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   // Gọi các hàm để tạo các biểu đồ
-  makeSegmentPrice("area", "ha_noi", false, 0, 0, 96.3, 95, "segment");
   makeSegmentPrice("area", "ha_noi", false, 0, 0, 96.3, 95, "segment1");
-  makeHistoryPrice("area", "ha_noi", true, 0, 0, 96.3, 95, "history");
-  makeCorrelation("area", "ha_noi", true, 0, 0, 96.3, 95, "correlation");
-  makeNumPropertyType("area", "ha_noi", true, 0, 0, 96.3, 95, "type");
 
   // Hàm vẽ biểu đồ cho StdDev và Variance
   function drawStdAndVarChart(district) {
